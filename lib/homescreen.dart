1c6322c8c7dba8b19d0acf1ed3e5db682f2e866b
omborate100 <omcode100@gmail.com>
2023-11-23 13:24:23 +0530

    animation added

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,5 +1,8 @@
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
 import 'dart:math';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -7,15 +10,49 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
   int leftDice = 1;
   int rightDice = 2;
+  late AnimationController _controller;
+  late CurvedAnimation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animate();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
+  animate() {
+    _controller =
+        AnimationController(duration: Duration(milliseconds: 500), vsync: this);
+    // _controller.forward();
+    animation = CurvedAnimation(parent: _controller, curve: Curves.bounceOut);
+    animation.addListener(() {
+      setState(() {});
+      print(_controller.value);
+    });
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          leftDice = Random().nextInt(6) + 1;
+          rightDice = Random().nextInt(6) + 1;
+        });
+        print("completed");
+        _controller.reverse();
+      }
+    });
+  }
 
   void roll() {
-    setState(() {
-      leftDice = Random().nextInt(6)+1;
-      rightDice = Random().nextInt(6)+1;
-    });
+    _controller.forward();
   }
 
   @override
@@ -29,16 +66,25 @@ class _HomeScreenState extends State<HomeScreen> {
           Row(
             children: [
               Expanded(
-                  child: Padding(
-                padding: EdgeInsets.all(15),
-                child: Image(
-                    image: AssetImage('images/dice-six-faces-$leftDice.png')),
+                  child: GestureDetector(
+                onDoubleTap: roll,
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Image(
+                      height: 250 - (animation.value) * 250,
+                      image: AssetImage('images/dice-six-faces-$leftDice.png')),
+                ),
               )),
               Expanded(
-                  child: Padding(
-                padding: EdgeInsets.all(15),
-                child: Image(
-                    image: AssetImage('images/dice-six-faces-$rightDice.png')),
+                  child: GestureDetector(
+                onDoubleTap: roll,
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Image(
+                      height: 250 - (animation.value) * 250,
+                      image:
+                          AssetImage('images/dice-six-faces-$rightDice.png')),
+                ),
               ))
             ],
           ),
